e193b990f32ee558c0aa557d5f5e4f2f2740927e
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-04-12 16:46:37 +0300

    fix7

--- src/app/tasks/services/task-promise.service.ts
+++ src/app/tasks/services/task-promise.service.ts
@@ -59,7 +59,8 @@ export class TaskPromiseService {
             .toPromise()
             .then( response => <Task>response.json() )
             .catch( this.handleError );
-}
+  }
+
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error);
     return Promise.reject(error.message || error);
