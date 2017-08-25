6205c3ec6c61a3de38c0a638314813e5f2aa919e
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-08-25 10:37:18 +0300

    more details about request options

--- src/app/users/services/user-observable.service.ts
+++ src/app/users/services/user-observable.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse, HttpErrorResponse, HttpParams } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
 import './../../services/rxjs-extensions';
@@ -20,12 +20,58 @@ export class UserObservableService {
       .catch(this.handleError);
   }
 
-  getUser(id: number): Observable<User> {
-    return this.http.get(`${this.usersUrl}/${id}`)
-      .map(this.handleData)
-      .catch(this.handleError);
+  // Case 1 Handle Body {observe: 'body'}
+  // getUser(id: number): Observable<User> {
+  //   return this.http.get(`${this.usersUrl}/${id}`, {observe: 'body'})
+  //     .map(this.handleData1)
+  //     .catch(this.handleError);
+  // }
 
+  // private handleData1(response: HttpResponse<User>) {
+  //   const body = response;
+  //   return body || {};
+  // }
+  // End Case 1
+
+  // Case 2: Handle Response { observe: 'response' }
+  // getUser(id: number): Observable<User> {
+  //   return this.http.get<User>(`${this.usersUrl}/${id}`, {observe: 'response'})
+  //     .map(this.handleData2)
+  //     .catch(this.handleError);
+  // }
+
+  // private handleData2(response: HttpResponse<User>) {
+  //   console.log(response);
+  //   const body = response.body;
+  //   return body || {};
+  // }
+  // End Case 2
+
+  // Case 3: Specify HttpResponse Type get<T>
+  // getUser(id: number): Observable<User> {
+  //   return this.http.get<User>(`${this.usersUrl}/${id}`)
+  //     .map(this.handleData3)
+  //     .catch(this.handleError);
+  // }
+
+  // private handleData3(user: User) {
+  //   const body = user;
+  //   return body || {};
+  // }
+  // End Case 3
+
+  // Case 4: responseType: text
+  getUser(id: number): Observable<User> {
+    return this.http.get(`${this.usersUrl}/${id}`, {responseType: 'text'})
+      .map(this.handleData4)
+      .catch(this.handleError);
   }
+
+  private handleData4(user: string) {
+    const body = JSON.parse(user);
+    return body || {};
+  }
+  // End Case 3
 
   updateUser(user: User): Observable<User> {
     const url = `${this.usersUrl}/${user.id}`,
@@ -44,7 +90,15 @@ export class UserObservableService {
     const url = this.usersUrl,
       body = JSON.stringify(user),
       options = {
-        headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+        // можно передавать объект конструктору, а можно вызывать метод set
+        // приэтом класс является immutable, каждый раз возвращается новый инстанс.
+        headers: new HttpHeaders()
+          .set('Content-Type', 'application/json')
+          .set('Authorization', 'my-auth-key'),
+
+        // добавление URL Query параметров: this.usersUrl?id=3
+        params: new HttpParams()
+          .set('id', '3')
       };
 
     return this.http.post(url, body, options)
@@ -74,5 +128,22 @@ export class UserObservableService {
                         : 'Server error';
     console.error(errMsg);
     return Observable.throw(errMsg);
+  }
+
+  private handleError2(err: HttpErrorResponse) {
+    let errorMessage: string;
+
+    // A client-side or network error occurred.
+    if (err.error instanceof Error) {
+      errorMessage = `An error occurred: ${err.error.message}`;
+    }
+    // The backend returned an unsuccessful response code.
+    // The response body may contain clues as to what went wrong,
+    else {
+      errorMessage = `Backend returned code ${err.status}, body was: ${err.error}`;
+    }
+
+    console.error(errorMessage);
+    return Observable.throw(errorMessage);
   }
 }
