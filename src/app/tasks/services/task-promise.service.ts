f95c99cca3b9267bd93714a58cc61eb97d3b7114
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-02-20 11:12:30 +0200

    fix4

--- src/app/tasks/services/task-promise.service.ts
+++ src/app/tasks/services/task-promise.service.ts
@@ -10,28 +10,28 @@ import { Task } from './../models/task.model';
 export class TaskPromiseService {
   private tasksUrl = 'http://localhost:3000/tasks';
 
-  constructor(
-    private http: HttpClient
-  ) {}
+  constructor(private http: HttpClient) {}
 
   getTasks(): Promise<Task[]> {
-    return this.http.get(this.tasksUrl)
-            .toPromise()
-            .then( response => <Task[]>response )
-            .catch(this.handleError);
+    return this.http
+      .get(this.tasksUrl)
+      .toPromise()
+      .then(response => <Task[]>response)
+      .catch(this.handleError);
   }
 
   getTask(id: number): Promise<Task> {
-    return this.http.get(`${this.tasksUrl}/${id}`)
-            .toPromise()
-            .then( response => <Task>response )
-            .catch(this.handleError);
-  }
+    const url = `${this.tasksUrl}/${id}`;
 
+    return this.http
+      .get(url)
+      .toPromise()
+      .then(response => <Task>response)
+      .catch(this.handleError);
+  }
 
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error);
     return Promise.reject(error.message || error);
   }
-
 }
